7bb1eec1ee30f02be11be5431c1d46e6ebe5551f
usuarioo1 <nicolasgonzalezmoller@gmail.com>
2023-09-13 22:01:38 -0300

    añadiendo elemtos a js

--- JS/app.js
+++ JS/app.js
@@ -1,90 +1,61 @@
 import { fetchApi } from "./funciones.js";
 
-
 let codigo = [];
-
 let estacion = [];
- 
+let temperatura = [];
+let humedad = [];
+let estadoGeneral = [];
 
-async function renderData(){
+async function renderData() {
+    const dataApi = await fetchApi('https://api.gael.cloud/general/public/clima/');
+    console.log(dataApi);
 
-    const dataApi = await fetchApi('https://api.gael.cloud/general/public/clima/')
-
-    console.log(dataApi)
-
-    codigo = dataApi.map((cd) => cd.Codigo)
-
+    codigo = dataApi.map((cd) => cd.Codigo);
     console.log(codigo);
 
-    estacion = dataApi.map((cd1) => cd1.Estacion)
-
+    estacion = dataApi.map((cd1) => cd1.Estacion);
     console.log(estacion);
-   
-    const codEst = [{codigo}, {estacion}];
 
+    temperatura = dataApi.map((cd2) => cd2.Temp)
+    console.log(temperatura)
 
-    let html = "";
+    humedad = dataApi.map((cd3)=>cd3.Humedad)
+    console.log(humedad)
 
-    // aqui esta listo para hacer recorrido con FOR o split
+    estadoGeneral = dataApi.map((cd4)=>cd4.Estado)
+    console.log(estadoGeneral)
 
-        codEst.forEach(function(element, index){
-        html += "<option> " + estacion[1] + " </option>";
-        
+    const selectRegiones = document.getElementById('regiones'); // Selecciona el elemento select por ID
+
+    estacion.forEach(function (element) {
+        const option = document.createElement('option');
+        option.value = element; // Utiliza el nombre de la ciudad como valor
+        option.text = element; // Muestra el nombre de la ciudad como texto de la opción
+        selectRegiones.appendChild(option);
     });
 
-
- 
-    // aqui esta inserto el ID del select
-    document.querySelector('#regiones').innerHTML = html;
-
-
-    
-    const ctx = document.getElementById('myChart')
-
- 
+    const ctx = document.getElementById('myChart');
 
     new Chart(ctx, {
-
         type: 'bar',
-
         data: {
-
-          labels: Estacion,
-
-          datasets: [{
-
-            label: 'Climas de Chile',
-
-            data: magnitudes,
-
-            borderWidth: 1,
-
-            backgroundColor: backgroundColors,
-            
-            borderColor: borderColor
-
-          },]
-
+            labels: estacion, // Usar estacion en lugar de Estacion
+            datasets: [{
+                label: 'Climas de Chile',
+                data: [], // Agregar los datos apropiados aquí
+                borderWidth: 1,
+                backgroundColor: [], // Agregar los colores apropiados aquí
+                borderColor: [] // Agregar los colores de borde apropiados aquí
+            }]
         },
-
         options: {
-
-          scales: {
-
-            y: {
-
-              beginAtZero: true
-
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
-
-          }
-
         }
-
-      });
-
+    });
 }
+renderData();
 
- 
-
-renderData()
