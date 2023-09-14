22ce4a2f08ddd5b53cd6bf2ef02bc5a7d603dcc1
usuarioo1 <nicolasgonzalezmoller@gmail.com>
2023-09-13 22:42:20 -0300

    añadiendo boton

--- JS/app.js
+++ JS/app.js
@@ -19,33 +19,35 @@ async function renderData() {
     temperatura = dataApi.map((cd2) => cd2.Temp)
     console.log(temperatura)
 
-    humedad = dataApi.map((cd3)=>cd3.Humedad)
+    humedad = dataApi.map((cd3) => cd3.Humedad)
     console.log(humedad)
 
-    estadoGeneral = dataApi.map((cd4)=>cd4.Estado)
+    estadoGeneral = dataApi.map((cd4) => cd4.Estado)
     console.log(estadoGeneral)
 
-    const selectRegiones = document.getElementById('regiones'); // Selecciona el elemento select por ID
+    const selectRegiones = document.getElementById('regiones'); 
 
     estacion.forEach(function (element) {
         const option = document.createElement('option');
-        option.value = element; // Utiliza el nombre de la ciudad como valor
-        option.text = element; // Muestra el nombre de la ciudad como texto de la opción
+        option.value = element; 
+        option.text = element; 
         selectRegiones.appendChild(option);
     });
+
+    const pedirInfo = document.getElementById('buscar')//falta aun...
 
     const ctx = document.getElementById('myChart');
 
     new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: estacion, // Usar estacion en lugar de Estacion
+            labels: estacion, 
             datasets: [{
                 label: 'Climas de Chile',
-                data: [], // Agregar los datos apropiados aquí
+                data: [], //agregar datos de variables para actuvar el grafico
                 borderWidth: 1,
-                backgroundColor: [], // Agregar los colores apropiados aquí
-                borderColor: [] // Agregar los colores de borde apropiados aquí
+                backgroundColor: [], 
+                borderColor: [] 
             }]
         },
         options: {
